1a86f11dd8d46865041606b059dfc776925f6765
Sahil Abass <sahilabass@Sahils-MacBook-Air.local>
2026-04-28 18:03:34 +0530

    fix admin permissions

--- donation/admin.py
+++ donation/admin.py
@@ -3,3 +3,13 @@ from .models import FoodDonation, Request
 
 admin.site.register(FoodDonation)
 admin.site.register(Request)
+from django.contrib.auth.models import User
+
+try:
+    user = User.objects.get(username="admin")
+    user.is_staff = True
+    user.is_superuser = True
+    user.is_active = True
+    user.save()
+except:
+    pass
